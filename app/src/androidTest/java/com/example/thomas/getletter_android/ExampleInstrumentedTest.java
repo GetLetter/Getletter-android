85efa00acf649a2212bbb9582080d0caeeb18f02
AdrienZ <adrienzaganelli@gmail.com>
2017-12-19 21:10:23 +0100

    Change package name in order to make gAutocomplete works

--- app/src/androidTest/java/com/example/thomas/getletter_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/thomas/getletter_android/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.thomas.getletter_android;
+package com.android.getletter.getletter;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.thomas.getletter_android", appContext.getPackageName());
+        assertEquals("com.android.getletter.getletter", appContext.getPackageName());
     }
 }
--- app/src/main/java/Activity/LoginActivity.java
+++ app/src/main/java/Activity/LoginActivity.java
@@ -2,15 +2,10 @@ package Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.thomas.getletter_android.R;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.android.getletter.getletter.R;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 /**
@@ -18,8 +13,6 @@ import com.google.firebase.analytics.FirebaseAnalytics;
  */
 
 public class LoginActivity extends AppCompatActivity{
-    private FirebaseAnalytics mFirebaseAnalytics;
-
     private TextView logo, punchline, conditions;
     private Button btnLogin;
 
@@ -34,32 +27,6 @@ public class LoginActivity extends AppCompatActivity{
         punchline = (TextView) findViewById(R.id.punchline);
         conditions = (TextView) findViewById(R.id.conditions);
         btnLogin = (Button) findViewById(R.id.btn_login);
-
-        // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
-        final TextView testText = findViewById(R.id.address_str);
-
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                Log.i("ouiui", "Place: " + place);
-                testText.setText(place.getAddress());
-
-            }
-
-            @Override
-            public void onError(Status status) {
-                // TODO: Handle the error.
-                Log.i("nonon", "An error occurred: " + status);
-            }
-        });
-
-
     }
 
 }
