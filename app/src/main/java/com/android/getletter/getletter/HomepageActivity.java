642c7ccee992d86eca7504647f404277faf71f56
AdrienZ <adrienzaganelli@gmail.com>
2017-12-21 00:36:51 +0100

    Add intent to receiver

--- app/src/main/java/com/android/getletter/getletter/HomepageActivity.java
+++ app/src/main/java/com/android/getletter/getletter/HomepageActivity.java
@@ -1,10 +1,13 @@
 package com.android.getletter.getletter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.mikepenz.fastadapter.FastAdapter;
 import com.mikepenz.fastadapter.adapters.ItemAdapter;
@@ -26,6 +29,18 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        Button btnSend = (Button) findViewById(R.id.btn_send);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent RecipientIndent = new Intent(getBaseContext(), RecipientActivity.class);
+                startActivity(RecipientIndent);
+            }
+        });
+
+
     }
 
     public void fillrecycler(final List<Object> letterList) {
--- app/src/main/java/com/android/getletter/getletter/RecipientActivity.java
+++ app/src/main/java/com/android/getletter/getletter/RecipientActivity.java
@@ -1,8 +1,16 @@
 package com.android.getletter.getletter;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Thomas on 20/12/2017.
  */
 
-public class RecipientActivity {
+public class RecipientActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipient);
+    }
 }
